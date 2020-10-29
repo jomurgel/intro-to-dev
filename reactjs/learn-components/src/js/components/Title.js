fe6fa8727c6672b09a1cd94ededcf4a3323910ab
jomurgel <dev@jomurgel.com>
2020-10-29 08:07:24 -0600

    updates learn-components app

--- reactjs/learn-components/src/js/components/Title.js
+++ reactjs/learn-components/src/js/components/Title.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class Title extends React.Component {
 	/**
 	 * Set initial props.
 	 * @type {object}
+
 	 */
-	static defaultProps = {
-		text: 'Alternative Title',
+	static propTypes = {
+		text: PropTypes.string.isRequired,
 	};
 
 	render() {
